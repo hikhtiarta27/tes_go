fb40ad8ecb8fff750b4b96a9687c0bd27e330446
hasan ikhtiarta <hasan.ikhtiarta@gmail.com>
2022-01-20 17:27:55 +0700

    init

--- index.go
+++ index.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 	"sync"
 
 	"github.com/go-chi/chi/v5"
@@ -14,45 +13,10 @@ import (
 	_ "github.com/godror/godror"
 )
 
-func sendUser(userId int, ch chan<- int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	_, err := http.Get("https://gorest.co.in/public/v1/posts/" + strconv.Itoa(userId))
-	if err != nil {
-		log.Println("err handle it")
-	}
-	// defer resp.Body.Close()
-	// b, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	log.Println("err handle it")
-	// }
-	ch <- userId
-}
-
-func process() {
-	ch := make(chan int)
-
-	var wg sync.WaitGroup
-
-	for i := 0; i < 50; i++ {
-		wg.Add(1)
-		go sendUser(i, ch, &wg)
-	}
-	// close the channel in the background
-	go func() {
-		wg.Wait()
-		close(ch)
-	}()
-	// read from channel as they come in until its closed
-
-	for res := range ch {
-		fmt.Println(res)
-	}
-}
-
 type responseJson struct {
-	Success bool                 `json:"status"`
-	Message string               `json:"message"`
-	Data    []*TicketCategoryDao `json:"data"`
+	Success bool                   `json:"status"`
+	Message string                 `json:"message"`
+	Data    [][]*TicketCategoryDao `json:"data"`
 }
 
 type TicketCategoryDao struct {
@@ -60,13 +24,38 @@ type TicketCategoryDao struct {
 	DESCRIPTION string
 }
 
+func callApi(userId int, ch chan<- []*TicketCategoryDao, wg *sync.WaitGroup, db *sql.DB) {
+	defer wg.Done()
+
+	ticketCategoryList := make([]*TicketCategoryDao, 0)
+
+	q, err := db.Query("SELECT CATEGORY_ID, CATEGORY_DESCRIPTION FROM TICKET_CATEGORY")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for q.Next() {
+		ticketCategory := new(TicketCategoryDao)
+		if err := q.Scan(&ticketCategory.ID, &ticketCategory.DESCRIPTION); err != nil {
+			log.Fatal(err)
+		}
+
+		ticketCategoryList = append(ticketCategoryList, ticketCategory)
+
+	}
+
+	fmt.Println(userId)
+
+	ch <- ticketCategoryList
+}
+
 func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/synchronize", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/tmp", func(w http.ResponseWriter, r *http.Request) {
 
-		ticketCategoryList := make([]*TicketCategoryDao, 0)
+		// ticketCategoryList := make([]*TicketCategoryDao, 0)
 
 		db, _ := sql.Open("godror", `user="jne" password="JNEmerdeka123!" connectString="(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=34.101.218.194)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=pdbdev)))"`)
 
@@ -81,14 +70,51 @@ func main() {
 				log.Fatal(err)
 			}
 
-			ticketCategoryList = append(ticketCategoryList, ticketCategory)
+			// ticketCategoryList = append(ticketCategoryList, ticketCategory)
 
 		}
 
 		// process()
 
 		resp := &responseJson{}
-		resp.Data = ticketCategoryList
+		// resp.Data = ticketCategoryList
+		resp.Message = "Hallo"
+		resp.Success = true
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(resp)
+	})
+
+	r.Get("/synchronize", func(w http.ResponseWriter, r *http.Request) {
+
+		db, _ := sql.Open("godror", `user="jne" password="JNEmerdeka123!" connectString="(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=34.101.218.194)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=pdbdev)))"`)
+
+		ch := make(chan []*TicketCategoryDao)
+
+		var wg sync.WaitGroup
+
+		for i := 0; i < 30; i++ {
+			wg.Add(1)
+			go callApi(i, ch, &wg, db)
+		}
+
+		// close the channel in the background
+		go func() {
+			wg.Wait()
+			close(ch)
+		}()
+
+		// read from channel as they come in until its closed
+
+		resData := make([][]*TicketCategoryDao, 0)
+
+		for res := range ch {
+			resData = append(resData, res)
+		}
+
+		resp := &responseJson{}
+		resp.Data = resData
 		resp.Message = "Hallo"
 		resp.Success = true
 
